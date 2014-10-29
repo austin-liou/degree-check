943159e5ce82597fd48c055f8e81250f928a89f8
Austin Liou <alnc94@berkeley.edu>
2014-10-29 15:32:52 -0700

    implemented session management and calnet

--- server/api/login/login.controller.js
+++ server/api/login/login.controller.js
@@ -3,15 +3,14 @@
 var _ = require('lodash');;
 var request = require('request');
 var parseString = require('xml2js').parseString;
-var util = require('util');
 var User = require('../user/user.model');
 
 // CalNet Auth Logic
 exports.index = function(req, res) {
-    if (req.session) {
-        if (Date.now() > req.session.expires) {
-            delete req.session;
-            res.redirect('https://auth.berkeley.edu/cas/login?service=https://degree-checker.herokuapp.com/login');
+    if (req.session && req.session.uid) {
+        if (req.sessionOptions && Date.now() > req.sessionOptions.expires) {
+            req.session = null;
+            res.redirect('https://auth.berkeley.edu/cas/login?service=https://degree-checker.herokuapp.com/login')
         }
         else {
             User.find({uid: req.session.uid}, function (err, users) {
@@ -25,7 +24,6 @@ exports.index = function(req, res) {
     else {
         var url = req.url;
         var ticketPos = url.indexOf('ticket');
-        console.log(url);
         if (ticketPos == -1) {
             res.redirect('https://auth.berkeley.edu/cas/login?service=https://degree-checker.herokuapp.com/login');
         }
@@ -34,7 +32,7 @@ exports.index = function(req, res) {
             request('https://auth.berkeley.edu/cas/serviceValidate?service=https://degree-checker.herokuapp.com/login&ticket=' + ticket, function (error, response, body) {
                 parseString(response.body, function (err, result) {
                     if (result['cas:serviceResponse']['cas:authenticationSuccess']) {
-                        var uid = result['cas:serviceResponse']['cas:authenticationSuccess']['cas:user'];
+                        var uid = result['cas:serviceResponse']['cas:authenticationSuccess'][0]['cas:user'][0];
                         User.find({uid: uid}, function (err, users) {
                             if (err) {
                                 console.log(err);
@@ -44,11 +42,7 @@ exports.index = function(req, res) {
                                     if (err) {
                                         console.log(err);
                                     }
-                                    req.session.user = user;
                                 });
-                            }
-                            else {
-                                req.session.user = users[0];
                             }
                             req.session.uid = uid;
                             res.redirect('../scheduler');
--- server/app.js
+++ server/app.js
@@ -8,7 +8,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
-var session = require('express-session');
+var session = require('cookie-session');
+var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
 var fs = require('fs');
@@ -35,11 +36,10 @@ var server = require('http').createServer(app);
 var sess = {
     name: 'degree-checker-cookie',
     secret: 'vF24(#0ag_a54$zh{41;S#0vyM?{V4',
-    cookie: {
-        secure: true,
-        maxAge: 604800000
-    }
+    domain: '.degree-checker.herokuapp.com',
+    expires: new Date(Date.now() + 604800000)
 };
+app.use(cookieParser());
 app.use(session(sess));
 require('./config/express')(app);
 require('./routes')(app);
