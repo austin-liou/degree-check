9ab1c098bbc3b068064b11353a051341fb0d2217
Joshua Mak <jmak2015@gmail.com>
2014-11-05 19:25:43 -0800

    Add stricter search filter

--- client/app/admin/controllers/admin.search-students.controller.js
+++ client/app/admin/controllers/admin.search-students.controller.js
@@ -11,4 +11,17 @@ angular.module('degreeCheckApp')
     $scope.studentName = function(student) {
     	return student.name;
     };
+    $scope.filterSearch = function(name, email, uid) {
+    	name = name === undefined ? "" : name.toLowerCase();
+    	email = email === undefined ? "" : email.toLowerCase();
+    	uid = uid === undefined ? "" : uid.toLowerCase();
+    	return function(student) {
+    		try {
+    			if (name === student.name.substring(0, name.length).toLowerCase() && email === student.name.substring(0, email.length).toLowerCase()) {
+    				return true;
+    			}
+    		} catch(err) {}
+    		return false;
+    	};
+    }
   });
