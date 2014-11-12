a59d3eb7140de18f9c63c9723715c10f1eb57b80
Joshua Mak <joshmak@berkeley.edu>
2014-11-11 20:58:35 -0800

    Add comments

--- client/app/services/admin-schedule-service/admin-schedule-service.service.js
+++ client/app/services/admin-schedule-service/admin-schedule-service.service.js
@@ -4,8 +4,10 @@ angular.module('degreeCheckApp')
   .factory('adminScheduleService', function ($http) {
     var service = {};
 
+    //list of users
     service.students = [];
 
+    //GET all existing users from server
     service.getStudents = function () {
       return $http.get('/api/users/')
         .success(function (students) {
