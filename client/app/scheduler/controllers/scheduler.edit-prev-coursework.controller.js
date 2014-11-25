aef65f251bb79338c278314535b5f773649963ec
Joshua Mak <jmak2015@gmail.com>
2014-11-25 04:39:34 -0800

    Add edit prev coursework functionality

--- client/app/scheduler/controllers/scheduler.edit-prev-coursework.controller.js
+++ client/app/scheduler/controllers/scheduler.edit-prev-coursework.controller.js
@@ -1,8 +1,31 @@
 'use strict';
 
 angular.module('degreeCheckApp')
-  .controller('SchedulerEditPrevCourseworkCtrl', function ($scope, $modalInstance) {
+  .controller('SchedulerEditPrevCourseworkCtrl', function ($scope, $modalInstance, scheduleService, majorService) {
     console.log('SchedulerEditPrevCourseworkCtrl');
+    $scope.scheduleService = scheduleService;
+    $scope.majorService = majorService;
+    $scope.majorService.initMajorService(function (courses) {
+      $scope.allCourses = courses.map(function (elem) { return elem.name; });
+    });
+    $scope.prev_coursework = $scope.scheduleService.schedule.prev_coursework;
+    $scope.newClass = '';
+    
+    $scope.checkInput = function (event, id) {
+      // Check if enter
+      if (event.keyCode === 13) {
+          // Check if valid course
+          var index = $scope.allCourses.indexOf($scope.newClass);
+          if (index > -1) {
+            var courseObj = majorService.allCourses[index];
+            scheduleService.addCourse(id, courseObj);
+            $scope.newClass = '';
+            $scope.scheduleService.schedule.prev_coursework.push(courseObj);
+            $scope.prev_coursework = $scope.scheduleService.schedule.prev_coursework;
+            $scope.scheduleService.saveSchedule();
+          }
+      }
+    };
 
     $scope.done = function () {
         $modalInstance.close();
