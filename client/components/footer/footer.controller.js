7abc78f21cda77aeeb31f9f1d892db82e0714e9c
Joshua Mak <jmak2015@gmail.com>
2014-11-27 03:05:16 -0800

    Create empty modals for footer buttons

--- client/components/footer/footer.controller.js
+++ client/components/footer/footer.controller.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('degreeCheckApp')
-  .controller('FooterCtrl', function ($scope, $location) {
+  .controller('FooterCtrl', function ($scope, $modal, $location) {
+  	
+  	$scope.about = function () {
+        var modalInstance = $modal.open({
+            templateUrl: 'about.html',
+            size: 'md'
+        });
+    };
 
+    $scope.contact = function () {
+        var modalInstance = $modal.open({
+            templateUrl: 'contact.html',
+            size: 'md'
+        });
+    };
+
+    $scope.team = function () {
+        var modalInstance = $modal.open({
+            templateUrl: 'team.html',
+            size: 'md'
+        });
+    };
   });
