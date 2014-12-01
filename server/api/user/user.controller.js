e391b18465b1b5a1e23a466e8199471216ffa068
Mari Batilando <mari.batilando@gmail.com>
2014-11-30 22:13:16 -0800

    Finish user endpoint caching

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -2,17 +2,31 @@
 
 var _ = require('lodash');
 var User = require('./user.model');
+var cache = require('memory-cache');
 
 // Get list of users
 exports.index = function(req, res) {
+  var cached = cache.get('all-users');
+
+  if (cached) {
+    return res.json(cached);
+  }
+
   User.find(function (err, users) {
     if(err) { return handleError(res, err); }
+    cache.put('all-users', users, 10000);
     return res.json(200, users);
   });
 };
 
 // Get a single user
 exports.show = function(req, res) {
+  var cached = cache.get(req.params.uid);
+
+  if (cached) {
+    return res.json(cached);
+  }
+
   User.findOne({uid: req.params.uid}).populate('prev_coursework').exec(function (err, user) {
     if(err) { return handleError(res, err); }
     if(!user) { return res.send(404); }
@@ -21,6 +35,7 @@ exports.show = function(req, res) {
     User.populate(user, opts, function (err, user) {
         var opts = [{path: 'schedules.major.requirements.courses', model: 'Course'}];
         User.populate(user, opts, function(err, user) {
+            cache.put(req.params.uid, user, 10000);
             return res.json(user);
         });
     });
@@ -31,6 +46,8 @@ exports.show = function(req, res) {
 exports.create = function(req, res) {
   User.create(req.body, function(err, user) {
     if(err) { return handleError(res, err); }
+    cache.del('all-users');
+    cache.put(user.uid, user, 10000);
     return res.json(201, user);
   });
 };
@@ -38,6 +55,10 @@ exports.create = function(req, res) {
 // Updates an existing user in the DB.
 exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
+
+  cache.del(req.params.uid);
+  cache.del('all-users');
+
   User.findOne({uid: req.params.uid}, function (err, user) {
     if (err) { return handleError(res, err); }
     if(!user) { return res.send(404); }
@@ -45,6 +66,7 @@ exports.update = function(req, res) {
     var updated = _.merge(user, req.body, function(a,b) {
         return b;
     });
+
     updated.save(function (err) {
         User.findOne({uid: req.params.uid}).populate('prev_coursework').exec(function (err, user) {
             if(err) { return handleError(res, err); }
@@ -54,6 +76,7 @@ exports.update = function(req, res) {
             User.populate(user, opts, function (err, user) {
                 var opts = [{path: 'schedules.major.requirements.courses', model: 'Course'}];
                 User.populate(user, opts, function(err, user) {
+                    cache.put(user.uid, user, 10000);
                     return res.json(user);
                 });
             });
@@ -64,6 +87,9 @@ exports.update = function(req, res) {
 
 // Deletes a user from the DB.
 exports.destroy = function(req, res) {
+  cache.del(req.params.uid);
+  cache.del('all-users');
+
   User.findOne({uid: req.params.uid}, function (err, user) {
     if(err) { return handleError(res, err); }
     if(!user) { return res.send(404); }
