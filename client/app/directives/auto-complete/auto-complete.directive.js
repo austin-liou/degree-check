441b1fc48683f5f8bea5eac55ff181b9abe66c65
Austin Liou <alnc94@berkeley.edu>
2014-12-15 16:39:26 -0800

    making directive work

--- client/app/directives/auto-complete/auto-complete.directive.js
+++ client/app/directives/auto-complete/auto-complete.directive.js
@@ -1,19 +1,17 @@
 'use strict';
 
 angular.module('degreeCheckApp')
-  .directive('autoComplete', function ($timeout) {
+  .directive('autoComplete', function () {
     return {
       restrict: 'A',
       link: function (scope, element, attrs) {
-	            element.autocomplete({
-                	source: scope[attrs.uiItems],
-	                change: function() {
-	                    element.trigger('input');
-	                    //$timeout(function() {
-	                    //  element.trigger('input');
-	                    //}, 0);
-	                }
-           		});
-            }
+        element.autocomplete({
+          source: scope[attrs.uiItems],
+          delay: 250,
+          change: function(event, ui) {
+            element.trigger('input');
+          }
+        });
       }
+    }
   });
